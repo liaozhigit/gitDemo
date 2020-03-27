9bb5032ca92b8b7516c33dbf89b58f8f00a8d78d
liaozhigit <18682146878@qq.com>
2020-03-28 01:46:19 +0800

    又进行了异常修改

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,5 @@ public class Main {
         System.out.println("Hello World! -add contents");
         System.out.println("第二次修改！");
         System.out.println("第三次修改！");
-        System.out.println("第四次修改！");
     }
 }
