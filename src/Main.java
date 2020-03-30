8cd3815f33612e169a15f01a710c5c2804bd1a57
liaozhigit <18682146878@qq.com>
2020-03-30 22:45:03 +0800

    还原到 ：第二次修改

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,5 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World! -add contents");
         System.out.println("第二次修改！");
-        System.out.println("第三次修改！");
-        System.out.println("dev");
-        System.out.println("sit1");
     }
 }
