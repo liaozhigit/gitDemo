0b41c1ca3c5a8364da31c9f4c6b74aad4a07d168
liaozhigit <18682146878@qq.com>
2020-03-31 01:23:12 +0800

    reset 第三次修改

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World! -add contents");
-        System.out.println("第d！");
-        System.out.println("d");
+        System.out.println("第二次修改！");
+        System.out.println("第三次修改！");
     }
 }
